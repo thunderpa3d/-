6bb1d07d83ce2c6414462b655968cc5a4c0da837
thunderpa3d <thunderpa3d@gmail.com>
2025-11-12 19:57:49 +0300

    Update script.js

--- script.js
+++ script.js
@@ -144,7 +144,7 @@ class SmartContactApp {
         this.contacts = [];
         this.filteredContacts = [];
         this.config = {
-            githubUrl: 'https://raw.githubusercontent.com/thunderpa3d/-/main/CONTACTS.xlsx',
+            githubUrl: 'https://raw.githubusercontent.com/thunderpa3d/-/CONTACTS.xlsx',
             syncInterval: 300000, // 5 دقائق
             maxRetries: 3,
             retryDelay: 2000
@@ -920,4 +920,5 @@ function initializeAppAfterAuth() {
         }
     }, 200);
 }
+
 
